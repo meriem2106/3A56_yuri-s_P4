2b31db1419e94d60e18edb1c3c94f95619d38e3a
rihem09 <rihem.benaissa@esprit.tn>
2024-04-26 10:33:28 +0100

    pom fix

--- src/main/java/test/HelloApplication.java
+++ src/main/java/test/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("TuniWorld");
         stage.setScene(scene);
         stage.show();
-        stage.show();
+    ::
     }   
     public static void main(String[] args) {
         launch();
